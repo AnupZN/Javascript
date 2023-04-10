2e53ce40f4b509c9889bed12877848b0d77f1ac4
Anup Anmol <anupanmolhansda1@gmail.com>
2023-04-10 09:34:57 +0530

    updated TOC

--- script.js
+++ script.js
@@ -21,28 +21,31 @@
 
 $(document).ready(function() {
   // Find all headings within the entry-content div
-  var headings = $('.entry-content').find('h1, h2, h3, h4, h5, h6');
+  var headings = $('.entry-content').find('h2, h3');
   
   // Create a container for the table of contents and add it to the page
-  var tocContainer = $('<div class="toc-container"></div>');
-  var tocList
+  var tocContainer = $('<div class="anup-toc-container"></div>');
+  var tocList= $('<ul class="anup-toc-list"></ul>')
   $('.entry-content').prepend(tocContainer);
   
   // Loop through each heading and create a table of contents item with a scroll link
   headings.each(function(index) {
     // Get the heading text and level
+    var tocListItem= $('<li class="anup-toc-list-item"></li>')
     var headingText = $(this).text();
     var headingLevel = $(this).prop('tagName').toLowerCase();
     
     // Create a unique ID for the heading to use in the scroll link
-    var headingId = 'heading-' + index;
+    var headingId = headingText.trim().split(" ").join("-");
     $(this).attr('id', headingId);
     
     // Create the table of contents item with a link to the heading
-    var tocItem = $('<a class="toc-link toc-' + headingLevel + '" href="#' + headingId + '">' + headingText + '</a>');
+    var tocItem = $('<a class="anup-toc-link anup-toc-' + headingLevel + '" href="#' + headingId + '">' + headingText + '</a>');
     
     // Add the table of contents item to the container
-    tocContainer.append(tocItem);
+    tocListItem.append(tocItem);
+    tocList.append(tocListItem);
+    tocContainer.append(tocList);
     
   });
 });
